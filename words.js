20beff01de9e3a7a696f909febe7ad2966378881
LajefaDS <152064865+LajefaDS@users.noreply.github.com>
2024-03-27 12:58:43 +0700

    Percobaan

---
+++ words.js
@@ -0,0 +1,198 @@
+const wordList = [
+    {
+        word: "lajefa",
+        hint: "Nama Gw"
+    },
+    {
+        word: "pensil",
+        hint: "Alat Untuk Menulis"
+    },
+    {
+        word: "aku",
+        hint: "Bahasa Indonesia Watashi"
+    },
+    {
+        word: "venus",
+        hint: "Salah Satu Planet terdekat Dengan Bumi"
+    },
+    {
+        word: "gold",
+        hint: "Bahasa Inggris Emas"
+    },
+    {
+        word: "bumi",
+        hint: "Planet Berpenghuni"
+    },
+    {
+        word: "bolang",
+        hint: "Bocah Petualang"
+    },
+    {
+        word: "lemari",
+        hint: "untuk menyimpan Baju"
+    },
+    {
+        word: "laptop",
+        hint: "Yg mirip Komputer"
+    },
+    {
+        word: "bola",
+        hint: "Yg biasa di tenfsng oleh kaki"
+    },
+    {
+        word: "avatar",
+        hint: "Film pengendali 4 Elemen"
+    },
+    {
+        word: "png",
+        hint: "Nama Firmat Foto"
+    },
+    {
+        word: "sendal",
+        hint: "Alas Kaki"
+    },
+    {
+        word: "voly",
+        hint: "Olahraga Tangan Menggunakan Bola"
+    },
+    {
+        word: "chrome",
+        hint: "salah satu nama google"
+    },
+    {
+        word: "bangku",
+        hint: "tempat duduk"
+    },
+    {
+        word: "catur",
+        hint: "Game yg ada benteng,raja,ratu,pion,kuda dll"
+    },
+    {
+        word: "kursi",
+        hint: "Tempat Duduk"
+    },
+    {
+        word: "kertas",
+        hint: "didalam buku ada?"
+    },
+    {
+        word: "jpg",
+        hint: "format foto"
+    },
+    {
+        word: "kuning",
+        hint: "nama warna yg cerah"
+    },
+    {
+        word: "burung",
+        hint: "hewan yg mempunyai Sayap"
+    },
+    {
+        word: "you",
+        hint: "Bahasa Inggris Anda"
+    },
+    {
+        word: "java",
+        hint: "Bahasa Pemrograman"
+    },
+    {
+        word: "google",
+        hint: "mesin pencari informasi"
+    },
+    {
+        word: "bekasi",
+        hint: "Kota Patriot"
+    },
+    {
+        word: "excel",
+        hint: "salah satu Microsoft Office"
+    },
+    {
+        word: "tidak",
+        hint: "Bahasa Indonesia NO"
+    },
+    {
+        word: "batam",
+        hint: "Kota kelahiran gw"
+    },
+    {
+        word: "gitar",
+        hint: "Alat Musik yang di petik"
+    },
+    {
+        word: "lemari",
+        hint: "Tempat Menyimpan Baju"
+    },
+    {
+        word: "ayam",
+        hint: "Hewan Bertelur"
+    },
+    {
+        word: "mars",
+        hint: "Salah Satu nama Planet"
+    },
+    {
+        word: "dolar",
+        hint: "Mata Uang Amerika"
+    },
+    {
+        word: "email",
+        hint: "Mengurim Pesan menggunakan Akun"
+    },
+    {
+        word: "html",
+        hint: "Bahasa Pemrograman dalam membuat Website"
+    },
+    {
+        word: "air",
+        hint: "water"
+    },
+    {
+        word: "aqua",
+        hint: "Merek Air"
+    },
+    {
+        word: "bolang",
+        hint: "Bocah Petualang"
+    },
+    {
+        word: "aoi",
+        hint: "Bahasa Jepang warna Biru"
+    },
+    {
+        word: "jpeg",
+        hint: "Nama Format Gambar"
+    },
+    {
+        word: "bantal",
+        hint: "Saat Tidur Apa yg ditindih Kepala"
+    },
+    {
+        word: "key",
+        hint: "kunci dalam bahasa Inggris"
+    },
+    {
+        word: "luwak",
+        hint: "Salah Satu hewan Huruf depan dari L"
+    },
+    {
+        word: "motor",
+        hint: "kendaraan yg sering dipakai"
+    },
+    {
+        word: "mobil",
+        hint: "Kendaraan Roda Empat"
+    },
+    {
+        word: "getah",
+        hint: "Isi dalam Pohon"
+    },
+    {
+        word: "sapu",
+        hint: "Alat untuk menyapu"
+    },
+    {
+        word: "cctv",
+        hint: "Kamera Tersembunyi"
+    },
+]
